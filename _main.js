ae31afde3c7b97e586e3ba5383ba53dd10cde736
Casey Ydenberg <ydenberg@gmail.com>
2016-06-04 11:00:22 -0600

    Basic editor working

--- _main.js
+++ _main.js
@@ -1,25 +1,182 @@
 const React = require('react')
 const ReactDOM = require('react-dom');
 
-const draft = require('draft-js');
-const Editor = draft.Editor;
-const EditorState = draft.EditorState;
+const Draft = require('draft-js');
+const Editor = Draft.Editor;
+const EditorState = Draft.EditorState;
+const RichUtils = Draft.RichUtils;
+const DefaultDraftBlockRenderMap = Draft.DefaultDraftBlockRenderMap;
 
+// const {Map} = Immutable;
 
 const WikEditor = React.createClass({
   getInitialState: function() {
     return ({
       editorState: EditorState.createEmpty()
-    });
+    })
   },
+
   onChange: function(editorState) {
     this.setState({editorState});
   },
-  render() {
+
+  handleKeyCommand: function(command) {
     const editorState = this.state.editorState;
-    return <Editor editorState={editorState} onChange={this.onChange} />;
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return true;
+    }
+    return false;
+  },
+
+  toggleBlockType: function(blockType) {
+    this.onChange(
+      RichUtils.toggleBlockType(
+        this.state.editorState,
+        blockType
+      )
+    );
+  },
+
+  toggleInlineStyle: function(inlineStyle) {
+    this.onChange(
+      RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        inlineStyle
+      )
+    );
+  },
+
+  render: function() {
+    const editorState = this.state.editorState;
+
+    // If the user changes block type before entering any text, we can
+    // either style the placeholder or hide it. Let's just hide it now.
+    let className = 'RichEditor-editor';
+    var contentState = editorState.getCurrentContent();
+    if (!contentState.hasText()) {
+      if (contentState.getBlockMap().first().getType() !== 'unstyled') {
+        className += ' RichEditor-hidePlaceholder';
+      }
+    }
+
+    return (
+      <div className="RichEditor-root">
+        <BlockStyleControls
+          editorState={editorState}
+          onToggle={this.toggleBlockType}
+        />
+        <InlineStyleControls
+          editorState={editorState}
+          onToggle={this.toggleInlineStyle}
+        />
+        <div className={className} onClick={this.focus}>
+          <Editor
+            blockStyleFn={getBlockStyle}
+            editorState={editorState}
+            handleKeyCommand={this.handleKeyCommand}
+            onChange={this.onChange}
+            placeholder=""
+            ref="editor"
+            spellCheck={true}
+          />
+        </div>
+      </div>
+    );
   }
 });
+
+function getBlockStyle(block) {
+  switch (block.getType()) {
+    case 'blockquote': return 'RichEditor-blockquote';
+    default: return null;
+  }
+}
+
+const StyleButton = React.createClass({
+  getInitialState: function() {
+    return({});
+  },
+
+  onToggle: function(e) {
+    e.preventDefault();
+    this.props.onToggle(this.props.style);
+  },
+
+
+  render: function() {
+    let className = 'RichEditor-styleButton';
+    if (this.props.active) {
+      className += ' RichEditor-activeButton';
+    }
+
+    return (
+      <span className={className} onMouseDown={this.onToggle}>
+        {this.props.label}
+      </span>
+    );
+  }
+
+});
+
+
+const BLOCK_TYPES = [
+  {label: 'H2', style: 'header-two'},
+  {label: 'H3', style: 'header-three'},
+  {label: 'H4', style: 'header-four'},
+  {label: 'H5', style: 'header-five'},
+  {label: 'H6', style: 'header-six'},
+  {label: 'Blockquote', style: 'blockquote'},
+  {label: 'UL', style: 'unordered-list-item'},
+  {label: 'OL', style: 'ordered-list-item'}
+];
+
+const BlockStyleControls = (props) => {
+  const editorState = props.editorState;
+  const selection = editorState.getSelection();
+  const blockType = editorState
+    .getCurrentContent()
+    .getBlockForKey(selection.getStartKey())
+    .getType();
+
+  return (
+    <div className="RichEditor-controls">
+      {BLOCK_TYPES.map((type) =>
+        <StyleButton
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      )}
+    </div>
+  );
+};
+
+const INLINE_STYLES = [
+  {label: 'Bold', style: 'BOLD'},
+  {label: 'Italic', style: 'ITALIC'},
+  {label: 'Underline', style: 'UNDERLINE'}
+];
+
+const InlineStyleControls = (props) => {
+  var currentStyle = props.editorState.getCurrentInlineStyle();
+  return (
+    <div className="RichEditor-controls">
+      {INLINE_STYLES.map(type =>
+        <StyleButton
+          key={type.label}
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      )}
+    </div>
+  );
+};
 
 ReactDOM.render(
   <WikEditor />,
