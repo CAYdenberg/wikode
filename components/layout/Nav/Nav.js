f046423780bb760d179afc0aeea33f3bb0fef058
Casey Ydenberg <ydenberg@gmail.com>
2016-08-04 20:28:28 -0600

    Style adjustments

--- components/layout/Nav/Nav.js
+++ components/layout/Nav/Nav.js
@@ -3,23 +3,24 @@ const React = require('react');
 module.exports = (props) => {
   const menuContents = props.user ?
     (
-      <ul className="menu align-right">
+      <ul className="vertical medium-horizontal menu align-right">
         <li>{props.user}</li>
-        <li><a href="/user/logout/" className="button warning">Logout</a></li>
+        <li><a href="/user/logout/">Logout</a></li>
+        <li><a href="#" onClick={props.showNewWikodeModal}>Create a new document</a></li>
       </ul>
     ) :
     (
-      <ul className="menu align-right">
-        <li><a href="#" className="button success" onClick={props.showSignInModal}>Sign In</a></li>
-        <li><a href="#" className="button warning" onClick={props.showCreateUserModal}>Create an account</a></li>
-        <li><a href="#" className="button info" onClick={props.showNewWikodeModal}>Create a new document</a></li>
+      <ul className="vertical medium-horizontal menu align-right">
+        <li><a href="#" onClick={props.showSignInModal}>Sign In</a></li>
+        <li><a href="#" onClick={props.showCreateUserModal}>Create an account</a></li>
+        <li><a href="#" onClick={props.showNewWikodeModal}>Create a new document</a></li>
       </ul>
     )
 
   return (
     <nav className="top-bar">
       <div className="top-bar-title">
-        <a href="/">Site Title</a>
+        <a href="/">Wikode</a>
       </div>
       {menuContents}
     </nav>
